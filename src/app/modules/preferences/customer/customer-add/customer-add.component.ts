dae4effb9fbc5e012baff897e121fa5c4e44f4c1
Ryan Arqueza <arquezayan@yahoo.com>
2020-08-05 14:55:09 +0800

    add customer add

--- src/app/modules/preferences/customer/customer-add/customer-add.component.ts
+++ src/app/modules/preferences/customer/customer-add/customer-add.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/core/http/api.service';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-customer-add',
@@ -6,10 +9,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-add.component.scss']
 })
 export class CustomerAddComponent implements OnInit {
+  public minDate: Date = new Date('05/07/1950');
+  public maxDate: Date = new Date('05/27/2017');
+  public dateValue: Date = new Date('05/16/2017');
 
-  constructor() { }
+  submitted = false;
+  customerForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private apiService: ApiService,
+    public dialogRef: MatDialogRef<CustomerAddComponent>
+  ) {
+    this.customerForm = this.fb.group({
+      customerCode: [''],
+      customerName: ['', Validators.required],
+      address: ['', Validators.required],
+      salesRepresentative: [''],
+      terms: ['', Validators.required],
+      tinNumber: ['', Validators.required],
+      contractDate: [''],
+      beginningBalance: [''],
+      contactPerson: this.fb.group({
+        name: [''],
+        contactNo: [''],
+        primary: ['']
+      })
+    });
+  }
 
   ngOnInit(): void {
+
   }
+
+  get f() { return this.customerForm.controls; }
+  get g() { return (this.customerForm.get('contactPerson') as FormGroup).controls; }
+
+  onSubmit(form) {
+    this.submitted = true;
+
+    if (this.customerForm.invalid) {
+      return;
+    }
+
+    this.apiService.postRequest('/customer', form).subscribe(res => {
+      console.log('HTTP response ', res);
+      alert('Successfully added');
+      this.onClose();
+    }, err => console.log('HTTP Error ', err)
+    );
+  }
+
+  onClose() {
+    this.dialogRef.close();
+  }
+
 
 }
