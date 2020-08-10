1e6b14e1cfa9763db4046394198c552fe1bee91d
Ryan Arqueza <arquezayan@yahoo.com>
2020-08-10 14:40:16 +0800

    add payee add and payee edit module

--- src/app/modules/preferences/payee/payee-add/payee-add.component.ts
+++ src/app/modules/preferences/payee/payee-add/payee-add.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/core/http/api.service';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-payee-add',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./payee-add.component.scss']
 })
 export class PayeeAddComponent implements OnInit {
-
-  constructor() { }
+  submitted = false;
+  payeeForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private apiService: ApiService,
+    public dialogRef: MatDialogRef<PayeeAddComponent>
+  ) {
+    this.payeeForm = this.fb.group({
+      payeeName: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
+
   }
+
+  get f() { return this.payeeForm.controls; }
+
+  onSubmit(form) {
+    this.submitted = true;
+
+    if (this.payeeForm.invalid) {
+      return;
+    }
+
+    this.apiService.postRequest('/payee', form).subscribe(res => {
+      console.log('HTTP response ', res);
+      alert('Successfully added');
+      this.onClose();
+    }, err => console.log('HTTP Error ', err)
+    );
+  }
+
+  onClose() {
+    this.dialogRef.close();
+  }
+
 
 }
--- src/app/modules/preferences/payee/payee-edit/payee-edit.component.ts
+++ src/app/modules/preferences/payee/payee-edit/payee-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/core/http/api.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-payee-edit',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PayeeEditComponent implements OnInit {
 
-  constructor() { }
+  payeeForm: FormGroup;
+  row: any;
+  submitted = false;
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private apiService: ApiService,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+
+    public dialogRef: MatDialogRef<PayeeEditComponent>
+  ) {
+
+    this.payeeForm = this.fb.group({
+      payeeName: [this.data.row.payeeName, Validators.required],
+
+    });
   }
 
+  ngOnInit(): void {
+
+  }
+
+  get f() { return this.payeeForm.controls; }
+
+  onSubmit(form) {
+    this.submitted = true;
+
+    if (this.payeeForm.invalid) {
+      return;
+    }
+
+    const id = this.data.row.id;
+    this.apiService.updateRequest('/payee', id, form).subscribe(res => {
+      console.log('HTTP response ', res);
+      alert('Successfully Updated');
+      this.onClose();
+    }
+      , err => console.log('HTTP Error ', err)
+    );
+  }
+
+  onClose() {
+    this.dialogRef.close();
+  }
 }
