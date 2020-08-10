ea5abd7d2be85ae7576f94c1b7ba04f92af0913b
Ryan Arqueza <arquezayan@yahoo.com>
2020-08-10 15:04:17 +0800

    add payable components

---
+++ src/app/modules/accounting/payable/payable-add/payable-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payable-add',
+  templateUrl: './payable-add.component.html',
+  styleUrls: ['./payable-add.component.scss']
+})
+export class PayableAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/accounting/payable/payable-list/payable-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payable-list',
+  templateUrl: './payable-list.component.html',
+  styleUrls: ['./payable-list.component.scss']
+})
+export class PayableListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
