c1a4034e80df6c4b4b513355c30814ab446b3e76
Ryan Arqueza <arquezayan@yahoo.com>
2020-08-10 12:26:49 +0800

    add payee list module

--- src/app/modules/preferences/payee/payee-routing.module.ts
+++ src/app/modules/preferences/payee/payee-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PayeeEditComponent } from './payee-edit/payee-edit.component';
+import { PayeeAddComponent } from './payee-add/payee-add.component';
+import { PayeeListComponent } from './payee-list/payee-list.component';
+import { PagenotfoundComponent } from 'src/app/shared/pagenotfound/pagenotfound.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add', component: PayeeAddComponent },
+  { path: 'edit', component: PayeeEditComponent },
+  { path: '', component: PayeeListComponent },
+  { path: '**', component: PagenotfoundComponent },
+  // { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
