28bee52201f17dd81bfbd2570526be5001eac9c1
Ryan Arqueza <arquezayan@yahoo.com>
2020-08-10 15:02:28 +0800

    import shared,reactive,forms module

--- src/app/modules/accounting/credit-memo/credit-memo.module.ts
+++ src/app/modules/accounting/credit-memo/credit-memo.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CreditMemoRoutingModule } from './credit-memo-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    CreditMemoRoutingModule
+    CreditMemoRoutingModule,
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class CreditMemoModule { }
--- src/app/modules/accounting/receivable/receivable.module.ts
+++ src/app/modules/accounting/receivable/receivable.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReceivableRoutingModule } from './receivable-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ReceivableRoutingModule
+    ReceivableRoutingModule,
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class ReceivableModule { }
