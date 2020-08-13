440ae25f679c4514e4d931cabd19e43dc074b7eb
Ryan Arqueza <arquezayan@yahoo.com>
2020-08-13 14:51:16 +0800

    FIX: routes conficlits

--- src/app/config/menu.config.ts
+++ src/app/config/menu.config.ts
@@ -5,18 +5,18 @@ import { Injectable } from '@angular/core';
 export class MenuConfig {
     public defaults: any = {
         accounting: [
-            { title: 'Chart of Accounts', path: '/pages/accounting/coa' },
-            { title: 'Section 2', path: '/pages/preferences/bank' },
+            { title: 'Chart of Accounts', path: '/accounting/coa' },
+            { title: 'Section 2', path: '/preferences/bank' },
             { title: 'Section 3', path: 'Third section' },
         ],
         preferences: [
-            { title: 'Sales Representative', path: '/pages/preferences/sales' },
-            { title: 'Supplier', path: '/pages/preferences/supplier' },
-            { title: 'Customer', path: '/pages/preferences/customer' },
-            { title: 'Term', path: '/pages/preferences/term' },
-            { title: 'Bank', path: '/pages/preferences/bank' },
-            { title: 'Projects', path: '/pages/preferences/projects' },
-            { title: 'Payee', path: '/pages/preferences/payee' }
+            { title: 'Sales Representative', path: '/preferences/sales' },
+            { title: 'Supplier', path: '/preferences/supplier' },
+            { title: 'Customer', path: '/preferences/customer' },
+            { title: 'Term', path: '/preferences/term' },
+            { title: 'Bank', path: '/preferences/bank' },
+            { title: 'Projects', path: '/preferences/projects' },
+            { title: 'Payee', path: '/preferences/payee' }
         ]
     };
 
