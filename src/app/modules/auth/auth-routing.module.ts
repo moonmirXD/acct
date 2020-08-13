7ad6e83d681693fc3839aa6679a391b25701678c
Ryan Arqueza <arquezayan@yahoo.com>
2020-08-13 13:40:42 +0800

    FEAT: Add login

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PagenotfoundComponent } from 'src/app/shared/pagenotfound/pagenotfound.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: '**', component: PagenotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
