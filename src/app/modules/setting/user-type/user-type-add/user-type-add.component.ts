36f746c818a5eb0cb83558e1eb57d53c188b92fd
Ryan Arqueza <arquezayan@yahoo.com>
2020-08-19 11:16:46 +0800

    CHORE: Add user type add module

--- src/app/modules/setting/user-type/user-type-add/user-type-add.component.ts
+++ src/app/modules/setting/user-type/user-type-add/user-type-add.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/core/http/api.service';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-type-add',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserTypeAddComponent implements OnInit {
 
-  constructor() { }
+  submitted = false;
+  userTypeForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private apiService: ApiService,
+    public dialogRef: MatDialogRef<UserTypeAddComponent>
+  ) {
+    this.userTypeForm = this.fb.group({
+      name: ['', Validators.required],
+      code: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
+
+  }
+
+  get f() { return this.userTypeForm.controls; }
+  get g() { return (this.userTypeForm.get('contactPerson') as FormGroup).controls; }
+
+  onSubmit(form) {
+    this.submitted = true;
+
+    if (this.userTypeForm.invalid) {
+      return;
+    }
+
+    this.apiService.postRequest('/usertype', form).subscribe(res => {
+      console.log('HTTP response ', res);
+      alert('Successfully added');
+      this.onClose();
+    }, err => console.log('HTTP Error ', err)
+    );
+  }
+
+  onClose() {
+    this.dialogRef.close();
   }
 
 }
