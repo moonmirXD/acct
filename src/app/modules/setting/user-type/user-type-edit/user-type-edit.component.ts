23b3cb5587620615354717aff7f970c199e7c334
Ryan Arqueza <arquezayan@yahoo.com>
2020-08-19 14:29:28 +0800

    CHORE: Add user tpye edit module

--- src/app/modules/setting/user-type/user-type-edit/user-type-edit.component.ts
+++ src/app/modules/setting/user-type/user-type-edit/user-type-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/core/http/api.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-type-edit',
@@ -7,9 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserTypeEditComponent implements OnInit {
 
-  constructor() { }
+  userTypeForm: FormGroup;
+  row: any;
+  submitted = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private apiService: ApiService,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+
+    public dialogRef: MatDialogRef<UserTypeEditComponent>
+  ) {
+
+    this.userTypeForm = this.fb.group({
+      name: [this.data.row.name, Validators.required],
+      code: [this.data.row.code, Validators.required],
+    });
+  }
 
   ngOnInit(): void {
+
   }
+
+  get f() { return this.userTypeForm.controls; }
+
+  onSubmit(form) {
+    this.submitted = true;
+
+    if (this.userTypeForm.invalid) {
+      return;
+    }
+
+    const id = this.data.row.id;
+    this.apiService.updateRequest('/usertype', id, form).subscribe(res => {
+      console.log('HTTP response ', res);
+      alert('Successfully updated');
+      this.onClose();
+    }
+      , err => console.log('HTTP Error ', err)
+    );
+  }
+
+  onClose() {
+    this.dialogRef.close();
+  }
+
 
 }
