a1a5205d3d86eaee5d87985329af7b52cdb7c31e
Ryan Arqueza <arquezayan@yahoo.com>
2020-09-01 14:16:55 +0800

    CHORE: Add routing and profile component

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
+import { ProfileViewComponent } from './profile-view/profile-view.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProfileViewComponent],
   imports: [
     CommonModule,
     ProfileRoutingModule
