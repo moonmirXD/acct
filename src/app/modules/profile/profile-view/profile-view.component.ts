ecbe43af641560b0718c264e5d25752ad3a0ec59
Ryan Arqueza <arquezayan@yahoo.com>
2020-09-01 14:52:00 +0800

    CHORE: Add get profile function

--- src/app/modules/profile/profile-view/profile-view.component.ts
+++ src/app/modules/profile/profile-view/profile-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/core/http/api.service';
 
 @Component({
   selector: 'app-profile-view',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-view.component.scss']
 })
 export class ProfileViewComponent implements OnInit {
-
-  constructor() { }
+  profileData: any;
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
   }
 
+
+  getProfile() {
+    this.apiService.getRequest('/profile').subscribe((res: any) => {
+      this.profileData = res;
+
+      console.log('HTTP response ', res);
+    }, err => console.log('HTTP Error ', err));
+  }
 }
