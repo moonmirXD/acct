3b73dc80c6a7e9c76d456da0e0ef924968b4593f
Ryan Arqueza <arquezayan@yahoo.com>
2020-09-01 14:17:24 +0800

    FIX: remove page-view component import

--- src/app/modules/pages.module.ts
+++ src/app/modules/pages.module.ts
@@ -4,11 +4,10 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
-import { ProfileViewComponent } from './profile-view/profile-view.component';
 
 
 @NgModule({
-  declarations: [PagesComponent, ProfileViewComponent],
+  declarations: [PagesComponent],
   imports: [
     CommonModule,
     PagesRoutingModule,
