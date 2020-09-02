7d2bdc32981a1e75606e908ce55d3b73a49860f0
Ryan Arqueza <arquezayan@yahoo.com>
2020-09-02 17:12:44 +0800

    CHORE: Add profile password component

--- src/app/modules/profile/profile.module.ts
+++ src/app/modules/profile/profile.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileViewComponent } from './profile-view/profile-view.component';
 import { ProfileEditComponent } from './profile-edit/profile-edit.component';
+import { ProfilePasswordComponent } from './profile-password/profile-password.component';
 
 
 @NgModule({
-  declarations: [ProfileViewComponent, ProfileEditComponent],
+  declarations: [ProfileViewComponent, ProfileEditComponent, ProfilePasswordComponent],
   imports: [
     CommonModule,
     ProfileRoutingModule
