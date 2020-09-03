5fa31b2b9cff97536fe30cd670cbc805944f0d66
Ryan Arqueza <arquezayan@yahoo.com>
2020-09-03 17:10:22 +0800

    CHORE: Add accounts activation and routes

---
+++ src/app/modules/account-activate/account-activate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-account-activate',
+  templateUrl: './account-activate.component.html',
+  styleUrls: ['./account-activate.component.scss']
+})
+export class AccountActivateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/pages.module.ts
+++ src/app/modules/pages.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
+import { AccountActivateComponent } from './account-activate/account-activate.component';
 
 
 @NgModule({
-  declarations: [PagesComponent],
+  declarations: [PagesComponent, AccountActivateComponent],
   imports: [
     CommonModule,
     PagesRoutingModule,
