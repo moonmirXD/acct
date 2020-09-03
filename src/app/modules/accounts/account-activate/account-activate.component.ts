aa0375429ea5185e537e825a51fe3645f9722bb8
Ryan Arqueza <arquezayan@yahoo.com>
2020-09-03 17:15:40 +0800

    CHORE: Add get account method

--- src/app/modules/accounts/account-activate/account-activate.component.ts
+++ src/app/modules/accounts/account-activate/account-activate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/core/http/api.service';
 
 @Component({
   selector: 'app-account-activate',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-activate.component.scss']
 })
 export class AccountActivateComponent implements OnInit {
-
-  constructor() { }
+  accountData: any;
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.getAccount();
+  }
+
+  getAccount() {
+    this.apiService.getRequest('/account').subscribe(res => {
+      this.accountData = res;
+    });
+  }
+  getAccountYear() {
+    this.apiService.getRequest('/accoun/choose/').subscribe(res => {
+      this.accountData = res;
+    });
   }
 
 }
