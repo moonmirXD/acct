59997c5c41e59588c412f4abe3b57442632bdea4
Ryan Arqueza <arquezayan@yahoo.com>
2020-09-04 13:49:31 +0800

    CHORE: Generate account create and manage

--- src/app/modules/accounts/accounts.module.ts
+++ src/app/modules/accounts/accounts.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AccountsRoutingModule } from './accounts-routing.module';
 import { AccountActivateComponent } from './account-activate/account-activate.component';
+import { AccountCreateComponent } from './account-create/account-create.component';
+import { AccountManageComponent } from './account-manage/account-manage.component';
 
 
 @NgModule({
-  declarations: [AccountActivateComponent],
+  declarations: [AccountActivateComponent, AccountCreateComponent, AccountManageComponent],
   imports: [
     CommonModule,
     AccountsRoutingModule
