e4831f009e119c7bfaad00406012175b42b7e8dd
Ryan Arqueza <arquezayan@yahoo.com>
2020-09-04 13:52:11 +0800

    CHORE: create and manage routes add

--- src/app/modules/accounts/accounts-routing.module.ts
+++ src/app/modules/accounts/accounts-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccountActivateComponent } from './account-activate/account-activate.component';
+import { PagenotfoundComponent } from 'src/app/shared/pagenotfound/pagenotfound.component';
+import { AccountManageComponent } from './account-manage/account-manage.component';
+import { AccountCreateComponent } from './account-create/account-create.component';
 
 
 const routes: Routes = [
   { path: '', component: AccountActivateComponent },
+  { path: 'manage', component: AccountManageComponent },
+  { path: 'create', component: AccountCreateComponent },
+  { path: '**', component: PagenotfoundComponent },
 ];
 
 @NgModule({
