d6878ac4cac666b09d208968a06005d45206abf6
Ryan Arqueza <arquezayan@yahoo.com>
2020-09-04 15:16:09 +0800

    CHORE: Add UI and formbuilder to add account

--- src/app/modules/accounts/account-create/account-create.component.ts
+++ src/app/modules/accounts/account-create/account-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/core/http/api.service';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-account-create',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountCreateComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  submitted = false;
+  accountForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private apiService: ApiService,
+    public dialogRef: MatDialogRef<AccountCreateComponent>
+  ) {
+    this.accountForm = this.fb.group({
+      year: ['', Validators.required],
+      name: ['', Validators.required],
+    });
   }
 
+  ngOnInit(): void {
+
+  }
+
+  get f() { return this.accountForm.controls; }
+  get g() { return (this.accountForm.get('name') as FormGroup).controls; }
+
+  onSubmit(form) {
+    this.submitted = true;
+
+    if (this.accountForm.invalid) {
+      return;
+    }
+
+    this.apiService.postRequest('/user/register/superadmin', form).subscribe(res => {
+      console.log(res);
+      alert('Successfully added');
+      this.onClose();
+    });
+  }
+
+  onClose() {
+    this.dialogRef.close();
+  }
 }
