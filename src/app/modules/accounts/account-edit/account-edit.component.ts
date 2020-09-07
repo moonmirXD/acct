504527f4a6f92fa8e570f280e8a4bd612ecf68fb
Ryan Arqueza <arquezayan@yahoo.com>
2020-09-07 16:57:25 +0800

    CHORE: Add account edit form UI w/ functions

--- src/app/modules/accounts/account-edit/account-edit.component.ts
+++ src/app/modules/accounts/account-edit/account-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ApiService } from 'src/app/core/http/api.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-account-edit',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountEditComponent implements OnInit {
 
-  constructor() { }
+  accountForm: FormGroup;
+  row: any;
+  submitted = false;
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private apiService: ApiService,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+
+    public dialogRef: MatDialogRef<AccountEditComponent>
+  ) {
+
+    this.accountForm = this.fb.group({
+      name: [this.data.row.name, Validators.required],
+      year: [this.data.row.year],
+      isClosed: [this.data.row.isClosed],
+    });
   }
 
+  ngOnInit(): void {
+
+  }
+
+  get f() { return this.accountForm.controls; }
+
+  onSubmit(form) {
+    this.submitted = true;
+
+    if (this.accountForm.invalid) {
+      return;
+    }
+
+    const id = this.data.row.id;
+    this.apiService.updateRequest('/sample', id, form).subscribe(res => {
+      console.log('HTTP response ', res);
+      alert('Successfully Updated');
+      this.onClose();
+    }
+      , err => console.log('HTTP Error ', err)
+    );
+  }
+
+  onClose() {
+    this.dialogRef.close();
+  }
 }
