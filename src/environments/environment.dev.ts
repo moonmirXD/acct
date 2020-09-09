b0b3d8fa4168698a0e9cd65898e7cb1b7099369e
Ryan Arqueza <arquezayan@yahoo.com>
2020-09-09 13:55:05 +0800

    CHORE: Add dev env(staging)

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    version: 'v1.0.0',
+    name: 'DEV ENV',
+    systemName: 'Accounting Project 022',
+    API_URL: 'http://localhost:3000'
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   version: 'v1.0.0',
-  name: 'DEV ENV',
+  name: 'PROD ENV',
   systemName: 'Accounting Project 022',
   // API_URL: 'http://localhost:3000'
   API_URL: 'https://accounting-test-app.herokuapp.com'
