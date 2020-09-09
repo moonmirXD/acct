b4785fc79190ed383f0ddc74f04aaf5ab936ec9d
Ryan Arqueza <arquezayan@yahoo.com>
2020-09-09 13:17:30 +0800

    FIX: Remove overlapping memory

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   version: 'v1.0.0',
   name: 'DEV ENV',
   systemName: 'Accounting Project 022',
